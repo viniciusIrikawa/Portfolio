62792812be274e35bbf6e761d41b10d92a3ad3c3
viniciusIrikawa <viniciusiandrade@hotmail.com>
2022-05-30 17:18:49 -0300

    Javascript bug fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,17 @@
-const header = document.querySelector(".nav-menu")
+const header = document.querySelector(".wrapper-list")
 document.addEventListener('scroll', () => {
-    let value = window.scrollY;
+    let value = window.pageYOffset;
     value.toFixed(0)
 
-    if (value >= 30) {
-        header.style.backdropFilter = 'blur(10px)';
-        header.style.boxShadow = ' 5px 5px 10px rgba(0, 0, 0, 0.3)'
-
-    }else if(value === 0){
-        header.style.background = 'transparent'
-        header.style.boxShadow = 'initial'
+    if (window.innerWidth > 768) {
+        if (value >= 30) {
+            header.style.backdropFilter = 'blur(10px)';
+            header.style.boxShadow = ' 5px 5px 10px rgba(0, 0, 0, 0.3)'
+            header.style.borderRadius = '0 0 15px 15px'
+            
+        }else if(value === 0){
+            header.style.boxShadow = 'initial'
+            header.style.borderRadius = 'initial'
+        }
     }
 })
